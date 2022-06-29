a5c73f3054c733a6190c9c37e60088d29a49a983
juanzacca <104774360+juanzacca@users.noreply.github.com>
2022-06-29 14:47:57 -0300

    Modificacion de errores

--- fantasmas.py
+++ fantasmas.py
@@ -579,3 +579,290 @@ def CrearFantasmas(screen, a):
         a[fantasmasimgy[f] // 50][fantasmasimgx[f] // 50] = 3
 
 
+#Busca valores (x,y) para un nuevo fantasma
+def Buscar(matrix, ancho,alto):
+
+    #valores iniciales
+    valorfila=random.randint(0, (alto // 50)-1)
+    valorcolum=random.randint(0, (ancho // 50)-1)
+
+    # valores finales
+    (filaescogida, columnaescogida) = (0, 0)
+
+    #si encontro el numero adecuado
+    encontrado=False
+    #si se cumple la distancia de link
+    lejos=False
+
+    # para mirar si esta en un borde del mapa
+    (bordearr, bordeaba, bordeder, bordeizq) = (False, False, False, False)
+
+
+#mirar si esta en un borde del mapa
+    # Esta arriba max
+    if valorfila == 0:
+        bordearr = True
+
+    # Esta abajo Max
+    if valorfila == (alto // 50) - 1:
+        bordeaba = True
+
+    # Esta izquierda Max
+    if valorcolum == 0:
+        bordeizq = True
+
+    # Esta Derecha Max
+    if valorcolum == (ancho // 50) - 1:
+        bordeder = True
+
+
+#mirar si esta en dos bordes al mismo tiempo
+    #arriba y a un lado
+    if (bordearr == True and bordeder == True) or (bordearr == True and bordeizq == True):
+        if bordearr == True and bordeder == True:
+            #print("arriba y a la derecha")
+            if (matrix[0][valorcolum-1] != 2) and (matrix[1][valorcolum-1] != 2) and (matrix[1][valorcolum] != 2) :
+                #print("si esta lejos link")
+                lejos=True
+        elif bordearr == True and bordeizq == True:
+            #print("arriba y a la izquierda")
+            if (matrix[0][1] != 2) and (matrix[1][1] != 2) and (matrix[1][0] != 2) :
+                #print("si esta lejos link")
+                lejos=True
+
+    #abajo y a un lado
+    if (bordeaba == True and bordeder == True) or (bordeaba == True and bordeizq == True):
+        if bordeaba == True and bordeder == True:
+            #print("abajo y a la derecha")
+            if (matrix[valorfila][valorcolum-1] != 2) and (matrix[valorfila-1][valorcolum-1] != 2) and (matrix[valorfila-1][valorcolum] != 2) :
+                #print("si esta lejos link")
+                lejos=True
+        elif bordeaba == True and bordeizq == True:
+            #print("abajo y a la izquierda")
+            if (matrix[valorfila][1] != 2) and (matrix[valorfila-1][1] != 2) and (matrix[valorfila-1][0] != 2) :
+                #print("si esta lejos link")
+                lejos=True
+
+#mirar si esta solo en un borde
+    # si esta arriba solamente
+    if bordearr == True and bordeaba == False and bordeder == False and bordeizq == False:
+        #print("solamente arriba")
+        if (matrix[0][valorcolum - 1] != 2) and (matrix[0][valorcolum + 1] != 2) and (matrix[1][valorcolum-1] != 2) \
+                and (matrix[1][valorcolum] != 2) and (matrix[1][valorcolum+1] != 2):
+            #print("si esta lejos link")
+            lejos = True
+
+    # si esta abajo solamente
+    if bordeaba == True and bordearr == False and bordeder == False and bordeizq == False:
+        #print("solamente abajo")
+        if (matrix[valorfila][valorcolum-1] != 2) and (matrix[valorfila][valorcolum+1] != 2) \
+                and (matrix[valorfila - 1][valorcolum-1] != 2) and (matrix[valorfila - 1][valorcolum] != 2) and (matrix[valorfila - 1][valorcolum+1] != 2):
+            #print("si esta lejos link")
+            lejos = True
+
+
+    # si esta a la izquierda solamente
+    if bordeizq == True and bordeaba == False and bordeder == False and bordearr == False:
+        #print("solamente izquierda")
+        if (matrix[valorfila-1][0] != 2) and (matrix[valorfila+1][0] != 2) and (matrix[valorfila - 1][valorcolum + 1] != 2)\
+                and (matrix[valorfila][valorcolum+1] != 2) and (matrix[valorfila + 1][valorcolum + 1] != 2):
+            #print("si esta lejos link")
+            lejos = True
+
+    # si esta a la derecha solamente
+    if bordeder == True and bordeaba == False and bordearr == False and bordeizq == False:
+        #print("solamente derecha")
+        if (matrix[valorfila-1][valorcolum] != 2) and (matrix[valorfila+1][valorcolum] != 2) and (matrix[valorfila - 1][valorcolum - 1] != 2)\
+                and (matrix[valorfila][valorcolum-1] != 2) and (matrix[valorfila + 1][valorcolum - 1] != 2):
+            #print("si esta lejos link")
+            lejos = True
+
+#mirar si no esta en ningun borde
+    if bordeder == False and bordeaba == False and bordearr == False and bordeizq == False:
+        #print("no esta en ningun borde ")
+        if (matrix[valorfila-1][valorcolum-1] != 2) and (matrix[valorfila-1][valorcolum] != 2) and (matrix[valorfila - 1][valorcolum + 1] != 2)\
+                and (matrix[valorfila][valorcolum+1] != 2) and (matrix[valorfila ][valorcolum - 1] != 2) and (matrix[valorfila+1][valorcolum-1] != 2) \
+                and (matrix[valorfila+1][valorcolum] != 2) and (matrix[valorfila + 1][valorcolum + 1] != 2):
+            #print("si esta lejos link")
+            lejos = True
+
+
+
+    #pasar valores a #*50
+    valorfila = valorfila * 50
+    valorcolum = valorcolum * 50
+
+    if valorfila == alto:
+        valorfila = valorfila - 50
+
+    if valorcolum == ancho:
+        valorcolum = valorcolum - 50
+
+    if matrix[valorfila//50][valorcolum//50] == 0:
+        encontrado=True
+    elif matrix[valorfila//50][valorcolum//50] != 0:
+        encontrado=False
+    else:
+        encontrado=False
+
+
+
+    if encontrado == True and lejos == True:
+        (filaescogida, columnaescogida) = (valorfila, valorcolum)
+        return filaescogida,columnaescogida
+    elif encontrado == False or lejos == False:
+        return Buscar(matrix, ancho , alto)
+
+#mover fantasmas con la posicion de cada uno , la matriz del mapa, el ancho y alto de la ventana
+def MoverFantasma(fantasma, matrixobst, ancho , alto):
+    global filallave,columnallave,filapuerta,columnapuerta
+    #capturar si paso por llave o puerta
+    (hayllave,haypuerta)=(False,False)
+
+    #opciones de movimiento disponibles
+    opciones = []
+
+    #para escoger una direccion disponible
+    (arr, aba, der, izq) = (True, True, True, True)
+
+    #para mirar si esta en un borde del mapa
+    (bordearr, bordeaba, bordeder, bordeizq) = (False, False, False, False)
+
+#Capturo posicion fantasma
+    posicionc=fantasma.left//50
+    posicionf=fantasma.top//50
+#quedan como posiciones anteriores
+    posicioncvieja=posicionc
+    posicionfvieja=posicionf
+
+    #si paso por llave
+    #if matrixobst[posicionf][posicionc]==4:
+     #   print("paso por la llave")
+      #  pg.quit()
+
+
+
+
+
+
+#Miro si esta en un limite del mapa
+    #Esta arriba Max, no se puede mover hacia arriba
+    if posicionf == 0 :
+        #print("Esta Arriba")
+        arr=False
+        bordearr=True
+
+    # Esta abajo Max, no se puede mover hacia abajo
+    if posicionf == (alto//50)-1:
+        #print("Esta Abajo")
+        aba=False
+        bordeaba=True
+
+    # Esta izquierda Max, no se puede mover hacia la izquierda
+    if posicionc == 0:
+        #print("Esta a la Izquierda")
+        izq=False
+        bordeizq=True
+
+    # Esta Derecha Max, no se puede mover hacia la derecha
+    if posicionc == (ancho // 50) - 1:
+        #print("Esta a la Derecha")
+        der=False
+        bordeder=True
+
+#Miro si tiene obstaculos al rededor
+    #a la izquierda
+    if bordeizq == False :
+        if matrixobst[posicionf][posicionc-1] == 1 :
+            izq=False
+        else:
+            izq:True
+
+    #a la derecha
+    if bordeder == False :
+        if matrixobst[posicionf][posicionc+1] == 1 :
+            der=False
+        else:
+            der=True
+
+    #arriba
+    if bordearr == False :
+        if matrixobst[posicionf-1][posicionc] == 1 :
+            arr=False
+        else:
+            arr=True
+
+    #abajo
+    if bordeaba == False :
+        if matrixobst[posicionf+1][posicionc] == 1 :
+            aba=False
+        else:
+            aba=True
+
+#Agrego opciones de movimientos al array
+    if der == True:
+        opciones.append(1)
+    if aba == True:
+        opciones.append(2)
+    if izq == True:
+        opciones.append(3)
+    if arr == True:
+        opciones.append(4)
+
+    valor = random.randint(0, opciones.__len__()-1)
+    elegido=opciones[valor]
+    #print(opciones)
+
+    # movimiento despues de elegir el lado a donde se va a mover
+    # mover derecha
+    if elegido == 1 and fantasma.left <= ancho - 100:
+        fantasma.left += 50
+        # mover abajo
+    if elegido == 2 and fantasma.top <= alto - 100:
+        fantasma.top += 50
+        # mover izquierda
+    if elegido == 3 and fantasma.left >= 50:
+        fantasma.left += -50
+        # mover arriba
+    if elegido == 4 and fantasma.top >= 50:
+        fantasma.top += -50
+
+#capturo posiciones nuevas
+    posicioncnueva = fantasma.left // 50
+    posicionfnueva = fantasma.top // 50
+
+#modifico posiciones viejas con 0 = vacio, o con 4 = llave , o 5 = puerta
+    #print("nueva")
+    #print(posicionfnueva, posicioncnueva)
+
+    #actualizo si estaba la llave en la posicion vieja
+    if (filallave == posicionfvieja and columnallave == posicioncvieja) or (filapuerta == posicionfvieja and columnapuerta == posicioncvieja) :
+        if (filallave == posicionfvieja and columnallave == posicioncvieja):
+            matrixobst[posicionfvieja][posicioncvieja] = 4
+        elif (filapuerta == posicionfvieja and columnapuerta == posicioncvieja):
+            matrixobst[posicionfvieja][posicioncvieja] = 5
+    else :
+        matrixobst[posicionfvieja][posicioncvieja] = 0
+
+    #si me muevo a la llave o a la puerta dejo la matrix como estaba con la llave o puerta
+    if matrixobst[posicionfnueva][posicioncnueva] == 4 or matrixobst[posicionfnueva][posicioncnueva] == 5:
+        if matrixobst[posicionfnueva][posicioncnueva] == 4:
+            print("pase por la llave")
+            matrixobst[posicionfnueva][posicioncnueva] = 4
+            #(filallave,columnallave)=(posicionfnueva,posicioncnueva)
+
+        elif matrixobst[posicionfnueva][posicioncnueva] == 5:
+            print("pase por la puerta")
+            matrixobst[posicionfnueva][posicioncnueva] = 5
+            (filapuerta, columnapuerta) = (posicionfnueva, posicioncnueva)
+    else:
+        matrixobst[posicionfnueva][posicioncnueva] = 3
+
+
+    return fantasma, matrixobst
+
+
+
+
+if __name__ == "__main__":
+    main()
